5ddd6abe4a304597d2ee8a2153b57169ebb637fb
Izzatbek <abdusharipovizzat03@gmail.com>
2026-05-17 13:59:59 +0500

    ui refactored

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button.tsx";
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+            <Button>Click me</Button>
+        </div>
+    )
+}
+export default App;
--- frontend/src/pages/verify-otp/ui/verify-otp-page.tsx
+++
@@ -1,8 +0,0 @@
-const VerifyOtpPage = () => {
-    return (
-        <div>
-            <h1>Verify OTP</h1>
-        </div>
-    );
-}
-export default VerifyOtpPage;
