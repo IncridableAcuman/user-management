84c69295989d6f1269e2caf6750fa8b127ae2777
Izzatbek <abdusharipovizzat03@gmail.com>
2026-05-21 15:03:42 +0500

    user management added

--- backend/src/main/java/com/auth/backend/dto/user/UserResponse.java
+++ backend/src/main/java/com/auth/backend/dto/user/UserResponse.java
@@ -1,4 +1,45 @@
 package com.auth.backend.dto.user;
 
-public record UserResponse() {
+import com.auth.backend.entity.UserEntity;
+import com.auth.backend.entity.enums.Gender;
+import com.auth.backend.entity.enums.UserRole;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public record UserResponse(
+        Long id,
+        String firstName,
+        String lastName,
+        String username,
+        String email,
+        UserRole role,
+        Gender gender,
+        String phone,
+        boolean enabled,
+        String avatar,
+        Date birthDate,
+        String bio,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+
+) {
+    public static UserResponse from(UserEntity user){
+        return new UserResponse(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getRole(),
+                user.getGender(),
+                user.getPhone(),
+                user.isEnabled(),
+                user.getAvatar(),
+                user.getBirthDate(),
+                user.getBio(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
 }
--- backend/src/main/java/com/auth/backend/service/UserService.java
+++ backend/src/main/java/com/auth/backend/service/UserService.java
@@ -1,4 +1,44 @@
 package com.auth.backend.service;
 
+import com.auth.backend.constant.ResponseMessage;
+import com.auth.backend.dto.user.UserResponse;
+import com.auth.backend.entity.UserEntity;
+import com.auth.backend.exception.CustomNotFoundException;
+import com.auth.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserResponse getMe(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assert authentication != null;
+        UserEntity user = (UserEntity) authentication.getPrincipal();
+        assert user != null;
+        return UserResponse.from(user);
+    }
+    @Transactional
+    public void removeUser(Long id){
+        UserEntity user = userRepository.findById(id).orElseThrow(()-> new CustomNotFoundException(ResponseMessage.NOT_FOUND));
+        userRepository.delete(user);
+    }
+    @Transactional
+    public List<UserResponse> getList(){
+        List<UserEntity> list = userRepository.findAll();
+        return list
+                .stream()
+                .map(UserResponse::from).toList();
+    }
+    public UserResponse getUserById(Long id){
+        UserEntity user = userRepository.findById(id).orElseThrow(()-> new CustomNotFoundException(ResponseMessage.NOT_FOUND));
+        return UserResponse.from(user);
+    }
 }
